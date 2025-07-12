895e8f570bedf277400d985b38fc48c311e25cc1
unknown <maddynenninger@gmail.com>
2025-07-12 13:01:11 -0700

    logic for starting up the apps, as well as clearning data

--- main.py
+++ main.py
@@ -54,6 +54,7 @@ class Home(QWidget):
         return [group_data.get("name", group_key) for group_key, group_data in self.data.items()]
     
     def initUI(self):
+        self.input_group = QComboBox()
         self.input_box_path = QTextEdit()
         self.submit = QPushButton("Submit")
         self.input_type = QComboBox()
@@ -61,6 +62,7 @@ class Home(QWidget):
         self.input_box_title.setFixedHeight(30)
 
         self.input_type.addItems(TYPES)
+        self.input_group.addItems(self.get_group_display_names())
 
         self.master = QHBoxLayout()
         col1 = QVBoxLayout()
@@ -72,6 +74,8 @@ class Home(QWidget):
         col3.setSpacing(5)
 
         # Add widgets to columns
+        col1.addWidget(QLabel("Workstation Group:"))
+        col1.addWidget(self.input_group)
         col1.addWidget(QLabel("File Path:"))  # Add labels
         col1.addWidget(self.input_box_path)
         
@@ -102,10 +106,45 @@ class Home(QWidget):
         self.submit.clicked.connect(self.submit_path)
 
     def submit_path(self):
+        this_group = self.input_group.currentText()
         this_file_path = self.input_box_path.toPlainText()
         this_type = self.input_type.currentText()
         this_title = self.input_box_title.toPlainText()
-        print(f'you just sumbitted an entry of type: {this_type} with the title {this_title} which can be found at {this_file_path}')
+        print(f'you just sumbitted an entry of type: {this_type} with the title {this_title} which can be found at {this_file_path} and will be added to this group {this_group}')
+        self.add_item_to_group(this_group, this_file_path, this_type, this_title)
+    
+    def add_item_to_group(self, group, file_path, type, title):
+        item = {"title": title, "type":type, "path": file_path}
+        group_key = self.get_group_key_by_name(group)
+        if group_key in self.data:
+            self.data[group_key]["items"].append(item)
+            self.save_data(self.data)
+
+            self.input_box_path.clear()
+            self.input_box_title.clear()
+
+            print("here")
+
+            # just for testing this function
+            self.start_workgroup("Work Setup")
+            return True
+        
+        return False
+    
+        
+    
+    def get_group_key_by_name(self, display_name):
+        for group_key, group_data in self.data.items():
+            if group_data.get("name", group_key) == display_name:
+                return group_key
+        return None
+            
+    def start_workgroup(self, group):
+        group_key = self.get_group_key_by_name(group)
+
+        if group_key in self.data:
+            for item in self.data[group_key]["items"]:
+                os.startfile(item.get("path")) 
 
 if __name__ == "__main__":
     app = QApplication([])
