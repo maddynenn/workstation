0ce781d0f359278bae9354f6bf3d92ce623553ae
unknown <maddynenninger@gmail.com>
2025-07-15 16:35:02 -0700

    updated UI and removed debugging stuff

--- main.py
+++ main.py
@@ -1,4 +1,61 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QTextEdit, QComboBox, QPushButton, QLabel, QHBoxLayout, QVBoxLayout, QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QTextEdit, QComboBox, QPushButton, QLabel, QHBoxLayout, QVBoxLayout, QMessageBox, QGroupBox
+from open_types import *
+import json
+import os
+import webbrowser
+
+class Home(QWidget):
+    def __init__(self, file_path="app_data.json"):
+        super().__init__()
+
+        self.file_path = file_path
+        self.data = self.load_or_create_data()
+
+        self.settings()
+        self.initUI()
+        self.button_click()
+
+    def load_or_create_data(self):
+        if os.path.exists(self.file_path):
+            try:
+                with open(self.file_path, 'r') as file:
+                    return json.load(file)
+            except (json.JSONDecodeError, IOError) as e:
+                print(f"Error reading file {e}")
+                return self.create_default_data()
+        else:
+            default_data = self.create_default_data()
+            self.save_data(default_data)
+            return default_data
+
+    def create_default_data(self):
+        return {
+            "work_group": {
+                "name": "Work Setup",
+                "items": []
+            },
+            "personal_group": {
+                "name": "Personal Setup",
+                "items": []
+            }
+        }
+
+    def save_data(self, data=None):
+        if data is None:
+            data = self.data
+        try:
+            with open(self.file_path, 'w') as file:
+                json.dump(data, file, indent=2)
+            return True
+        except IOError as e:
+            print(f"Error saving file: {e}")
+            return False
+    
+    def get_group_display_names(self):
+        return [group_data.get("name", group_key) for group_key, group_data in self.data.items()]
+    
+    from PyQt5.QtWidgets import QApplication, QWidget, QTextEdit, QComboBox, QPushButton, QLabel, QHBoxLayout, QVBoxLayout, QMessageBox, QFrame, QGroupBox
+from PyQt5.QtCore import Qt
 from open_types import *
 import json
 import os
@@ -56,51 +113,47 @@ class Home(QWidget):
         return [group_data.get("name", group_key) for group_key, group_data in self.data.items()]
     
     def initUI(self):
+        main_layout = QVBoxLayout()
+        main_layout.setSpacing(15)
+        main_layout.setContentsMargins(20, 20, 20, 20)
+        
+        main_layout.addWidget(QLabel("Workstation Group:"))
         self.input_group = QComboBox()
-        self.input_box_path = QTextEdit()
-        self.submit = QPushButton("Submit")
-        self.input_type = QComboBox()
+        self.input_group.addItems(self.get_group_display_names())
+        main_layout.addWidget(self.input_group)
+        
+        main_layout.addWidget(QLabel("Title:"))
         self.input_box_title = QTextEdit()
         self.input_box_title.setFixedHeight(30)
-        self.run_button = QPushButton("Run Group")
-
+        main_layout.addWidget(self.input_box_title)
+        
+        type_path_row = QHBoxLayout()
+        
+        type_col = QVBoxLayout()
+        type_col.addWidget(QLabel("Type:"))
+        self.input_type = QComboBox()
         self.input_type.addItems(TYPES)
-        self.input_group.addItems(self.get_group_display_names())
+        type_col.addWidget(self.input_type)
+        type_col.addStretch()  # Push type dropdown to top
+        
+        path_col = QVBoxLayout()
+        path_col.addWidget(QLabel("File Path:"))
+        self.input_box_path = QTextEdit()
+        path_col.addWidget(self.input_box_path)
+        
+        type_path_row.addLayout(type_col)
+        type_path_row.addLayout(path_col)
+        main_layout.addLayout(type_path_row)
+        
+        button_row = QHBoxLayout()
+        self.submit = QPushButton("Submit")
+        self.run_button = QPushButton("Run Group")
+        button_row.addWidget(self.submit)
+        button_row.addWidget(self.run_button)
+        main_layout.addLayout(button_row)
 
-        self.master = QHBoxLayout()
-        col1 = QVBoxLayout()
-        col2 = QVBoxLayout()
-        col3 = QVBoxLayout()
-
-        col1.setSpacing(5)
-        col2.setSpacing(5)
-        col3.setSpacing(5)
-
-        # Add widgets to columns
-        col1.addWidget(QLabel("Workstation Group:"))
-        col1.addWidget(self.input_group)
-        col1.addWidget(QLabel("File Path:"))  # Add labels
-        col1.addWidget(self.input_box_path)
-        
-        col2.addWidget(QLabel("Type:"))
-        col2.addWidget(self.input_type)
-        
-        col3.addWidget(QLabel("Title:"))
-        col3.addWidget(self.input_box_title)
-        col3.addWidget(self.submit)
-        col3.addWidget(self.run_button)
-        
-        # Add stretch to push everything to top
-        col1.addStretch()
-        col2.addStretch()
-        col3.addStretch()
-        
-        # Add columns to master layout with equal proportions
-        self.master.addLayout(col1, 1)
-        self.master.addLayout(col2, 1)
-        self.master.addLayout(col3, 1)
-        
-        self.setLayout(self.master)
+        main_layout.addStretch()      
+        self.setLayout(main_layout)
 
     def settings(self):
         self.setWindowTitle("Workstation")
@@ -115,7 +168,6 @@ class Home(QWidget):
         this_file_path = self.input_box_path.toPlainText()
         this_type = self.input_type.currentText()
         this_title = self.input_box_title.toPlainText()
-        print(f'you just sumbitted an entry of type: {this_type} with the title {this_title} which can be found at {this_file_path} and will be added to this group {this_group}')
         self.add_item_to_group(this_group, this_file_path, this_type, this_title)
 
     
